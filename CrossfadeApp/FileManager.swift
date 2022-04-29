93eaf64423b073d799ea2a0c2cae66ad89fade7b
MartynovOleg <103255306+MartynovOleg@users.noreply.github.com>
2022-04-29 14:06:18 +0300

    123

---
+++ CrossfadeApp/FileManager.swift
@@ -0,0 +1,12 @@
+//
+//  FileManager.swift
+//  CrossfadeApp
+//
+//  Created by mac on 28.04.2022.
+//
+
+import UIKit
+import AVFoundation
+
+
+
--- CrossfadeApp/ViewController.swift
+++ CrossfadeApp/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer?
+    
+    var fileURL = Bundle.main.path(forResource: "Ark Patrol feat. Veronika Redd - Let Go", ofType: "mp3")
+   
+    @IBOutlet var audio1Botton: UIButton!
+    @IBOutlet var audio2Button: UIButton!
+    
+    @IBOutlet var audioNames: UILabel!
+    @IBOutlet var playButton: UIButton!
+    @IBAction func audio1DidTapped(_ sender: UIButton) {
+        playSound()
+        
+    }
+    
+    @IBAction func audio2DidTapped(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "Ark Patrol feat. Veronika Redd - Let Go", withExtension: "mp3") else { return }
+        self.audioNames.text = url.lastPathComponent
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
 
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
 
-}
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let audioPlayer = audioPlayer else {
+                return
+            }
+            audioPlayer.play()
+            audioPlayer.numberOfLoops = -1
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+   }
 
